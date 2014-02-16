ae0c443a65014b537424c557bfc9b1c21b024b1e
Mischov <mmischov@gmail.com>
2014-02-16 10:19:04 -0600

    Added Expectations deps.

--- project.clj
+++ project.clj
@@ -4,5 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
+                 [expectations "1.4.52"]
                  [ring/ring-core "1.2.1"]
-                 [org.clojure/tools.reader "0.8.3"]])
+                 [org.clojure/tools.reader "0.8.3"]]
+  :plugins [[lein-expectations "0.0.7"]])
