26b6b3b86805f7a012ad14dc26ccf547c22feedc
Mischov <mmischov@gmail.com>
2014-03-08 12:23:58 -0600

    Added Hello World example.

---
+++ examples/gate/examples/hello_world.clj
@@ -0,0 +1,46 @@
+(ns gate.examples.hello-world
+  (:require [gate :refer [defrouter]]
+            [gate.handler :refer [defhandler]]))
+
+;; A Gate handler is some function that accepts a Ring request.
+;;
+;; Gate tries to coerce whatever the handler returns into
+;; a Ring response.
+
+(defn hello-world
+  [request]
+  "Hello, World!")
+
+;; Because manually pulling parameters out of a request
+;; can get tedious, gate.handler provides a convenience
+;; macro, defhandler, to make it easier.
+;;
+;; In the following example, defhandler gets the :name key
+;; from the request :params and binds it to the symbol `name`.
+
+(defhandler hello-name
+  [name]
+  (str "Hello, " name "!"))
+
+;; A Gate router takes a list of routes and an optional map
+;; of options and returns a function that attempts to find
+;; a route matching a Ring request.
+
+(defrouter app
+  [{:name :hello-world
+    :path "/hello"
+    :get hello-world
+    :children [
+               {:name :hello-name
+                :path "/:name"
+                :get hello-name}]}]
+  {:404-handler (fn [_] "Nothing here to see! Carry on!")})
+
+;; (app {:uri "/hello" :request-method :get})
+;; => {:status 200, :headers {"Content-Type" "text/html; charset=utf-8"}, :body "Hello, World!"}
+
+;; (app {:uri "/hello" :request-method :post})
+;; => {:status 404, :headers {"content-type" "text/html; charset=utf-8"}, :body "Nothing here to see! Carry on!"}
+
+;; (app {:uri "/hello/Clojure" :request-method :get})
+;; => {:status 200, :headers {"Content-Type" "text/html; charset=utf-8"}, :body "Hello, Clojure!"}
