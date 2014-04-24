1042a2f02877523aa1db35bb994b1c75a526903a
Mischov <mmischov@gmail.com>
2014-04-24 11:44:57 -0500

    Version bump to 0.0.15.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject gate "0.0.14"
+(defproject gate "0.0.15"
   :description "Gate is a routing library for Clojure's Ring."
   :url "https://github.com/mischov/gate"
   :license {:name "Eclipse Public License"
